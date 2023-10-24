0b8f29921b452f466fbb56267fdb56089eeabd05
nastyakolosovskaya <nastyakolosovskaya@coherentsolutions.com>
2023-10-24 11:45:38 +0300

    Add Apache client, add Object mapper for response, improve singleton

--- src/main/java/storageApp/helpers/TokenSingleton.java
+++ src/main/java/storageApp/helpers/TokenSingleton.java
@@ -1,62 +1,34 @@
 package storageApp.helpers;
 
-import lombok.SneakyThrows;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
+import storageApp.data.HttpAuthenticationResponse;
 
 public class TokenSingleton {
 
-    private TokenSingleton() {
+    public static HttpAuthenticationResponse httpAuthenticationResponse;
+
+    TokenSingleton() {
     }
 
-    static TokenSingleton SINGLETON;
+    private static String SINGLETON_READ;
+    private static String SINGLETON_WRITE;
 
-    public static TokenSingleton getInstance(String value) {
-        if (SINGLETON == null) {
-            SINGLETON = new TokenSingleton();
-            SINGLETON.initialize(value);
+    private static HttpAuthenticateClient httpAuthenticateClient = new HttpAuthenticateClient();
+
+
+    public static HttpAuthenticationResponse initialize() {
+        if (httpAuthenticationResponse == null) {
+
+            SINGLETON_READ = httpAuthenticateClient.getHttpConnection("read").getAccess_token();
+            SINGLETON_WRITE = httpAuthenticateClient.getHttpConnection("write").getAccess_token();
+
         }
-        return SINGLETON;
+        return httpAuthenticationResponse;
     }
 
-    @SneakyThrows
-    private void initialize(String scopeValue) {
-
-        HttpAuthenticator httpAuthenticator = new HttpAuthenticator();
-        String encodedAuthorizedUser = httpAuthenticator.getAuthentication();
-        String urlParameters = "grant_type=client_credentials&scope=" + scopeValue;
-        HttpURLConnection conn = getHttpURLConnection(urlParameters, encodedAuthorizedUser);
-
-        int responseCode = conn.getResponseCode();
-        System.out.println("Response Code: " + responseCode);
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        System.out.println(response);
+    public static String getSingletonRead() {
+        return SINGLETON_READ;
     }
-    @SneakyThrows
-    private static HttpURLConnection getHttpURLConnection(String urlParameters, String encodedAuthorizedUser){
-        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
-        String request = "http://localhost:4044/oauth/token";
-        URL url = new URL(request);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setDoOutput(true);
-        conn.setInstanceFollowRedirects(false);
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Authorization", "Basic " + encodedAuthorizedUser);
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty("charset", "utf-8");
-        try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
-            wr.write(postData);
-        }
-        return conn;
+    public static String getSingletonWrite() {
+        return SINGLETON_WRITE;
     }
 }
